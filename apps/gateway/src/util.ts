f6e76d8b10fb2f30793a36917b46fde995689849
Nikki <bravonikki13@gmail.com>
2026-03-02 00:44:20 +0100

    refactor: remove unused parseShards utility function

--- apps/gateway/src/util.ts
+++
@@ -1,7 +0,0 @@
-export function parseShards(shardEnv: string): number[] {
-  if (shardEnv.includes('-')) {
-    const [start, end] = shardEnv.split('-').map(Number);
-    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-  }
-  return shardEnv.split(',').map(Number);
-}
