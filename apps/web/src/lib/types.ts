88e68bb952dbb59b29f16703036438efd9db9a62
Nikki <bravonikki13@gmail.com>
2026-03-04 01:07:54 +0100

    feat: add FilterType and FilterStatus enums for improved filtering capabilities

---
+++ apps/web/src/lib/types.ts
@@ -0,0 +1,12 @@
+export enum FilterType {
+  All = 'all',
+  Gateway = 'gateway',
+  Worker = 'worker',
+}
+
+export enum FilterStatus {
+  All = 'all',
+  Online = 'online',
+  Unresponsive = 'unresponsive',
+  Offline = 'offline',
+}
